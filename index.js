e13d2d0e76b8f4031e371f2de80fc4135aea7b18
Sayem92 <sayemmiha123@gmail.com>
2022-11-08 13:12:41 +0600

    get services data limit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -10,8 +11,36 @@ app.use(cors())
 app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+
+     const photographerCollection = client.db("Photographer").collection("services");
+
+
+        // get 3 services mongo--------
+        app.get('/services', async(req,res)=>{
+            const query = {}
+            const cursor = photographerCollection.find(query)
+            const services = await cursor.limit(3).toArray() 
+            res.send(services)
+        })
+
+
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+run().catch(err => console.log(err))
+
 app.get('/', (req,res)=>{
-    res.send('Server Candid Captures is Running')
+    res.send('Server Candid Captures is Running');
 });
 
 app.listen(port, console.log("server running port on:", port));
