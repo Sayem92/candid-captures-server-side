75b9e2a50c17f4a6f403b54b3b1deded73c53d74
Sayem92 <sayemmiha123@gmail.com>
2022-11-08 15:29:36 +0600

    load details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -22,13 +22,30 @@ async function run(){
      const photographerCollection = client.db("Photographer").collection("services");
 
 
-        // get 3 services mongo--------
+        // get 3 services data mongo--------
         app.get('/services', async(req,res)=>{
             const query = {}
             const cursor = photographerCollection.find(query)
             const services = await cursor.limit(3).toArray() 
             res.send(services)
+        });
+
+        // get all services data mongo--------
+        app.get('/allServices', async(req,res)=>{
+            const query = {}
+            const cursor = photographerCollection.find(query)
+            const allServices = await cursor.toArray() 
+            res.send(allServices)
         })
+
+        //details service-------
+        app.get('/details/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = { _id : ObjectId(id)}
+            const serDetails = await photographerCollection.findOne(query) 
+            res.send(serDetails)
+        })
+
 
 
     }
